3ed906ccaf77831a4925e8a207aa4472295bdd4c
Arpan Kumar <arpan.kumar@microchip.com>
2021-08-05 16:47:44 +0530

    [EVSYS] Regenerated demo for evsys_evsys_trigger_pic32cm_jh01_curiosity_pro
    MH3-56686
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/evsys/evsys_trigger/firmware/src/packs/PIC32CM5164JH01100_DFP/instance/osc32kctrl.h
+++ apps/evsys/evsys_trigger/firmware/src/packs/PIC32CM5164JH01100_DFP/instance/osc32kctrl.h
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2020-08-03T13:07:40Z */
+/* file generated from device description version 2021-02-12T15:11:42Z */
 #ifndef _PIC32CMJH01_OSC32KCTRL_INSTANCE_
 #define _PIC32CMJH01_OSC32KCTRL_INSTANCE_
 
