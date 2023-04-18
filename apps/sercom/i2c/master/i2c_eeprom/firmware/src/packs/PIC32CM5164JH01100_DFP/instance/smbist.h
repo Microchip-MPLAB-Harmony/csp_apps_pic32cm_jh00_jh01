7b35e4985ede51f810f254ab9863a0c9a449e4d6
Sumanth Naga Venkata Ranga Suraneni <Sumanth.Suraneni@microchip.com>
2023-04-18 18:08:31 +0530

    [SERCOM] Regenerated demo for sercom_i2c_master_i2c_eeprom_pic32cm_jh01_curiosity_pro
    MH3-77204

--- apps/sercom/i2c/master/i2c_eeprom/firmware/src/packs/PIC32CM5164JH01100_DFP/instance/smbist.h
+++ apps/sercom/i2c/master/i2c_eeprom/firmware/src/packs/PIC32CM5164JH01100_DFP/instance/smbist.h
@@ -1,7 +1,7 @@
 /*
  * Instance header file for PIC32CM5164JH01100
  *
- * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2023 Microchip Technology Inc. and its subsidiaries.
  *
  * Subject to your compliance with these terms, you may use Microchip software and any derivatives
  * exclusively with Microchip products. It is your responsibility to comply with third party license
@@ -20,14 +20,14 @@
  *
  */
 
-/* file generated from device description version 2022-01-18T09:31:06Z */
+/* file generated from device description version 2023-03-27T04:02:13Z */
 #ifndef _PIC32CMJH01_SMBIST_INSTANCE_
 #define _PIC32CMJH01_SMBIST_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for SMBIST peripheral ========== */
-#define SMBIST_EN_LMOB                           _UL_(0)    /* Enable Local SRAM MBIST On Boot. */
-#define SMBIST_EN_ONLINE                         _UL_(1)    /* Enable online Local SRAM MBIST. */
-#define SMBIST_INSTANCE_ID                       _UL_(91)   /* Instance index for SMBIST. */
+#define SMBIST_EN_LMOB                           (0)        /* Enable Local SRAM MBIST On Boot. */
+#define SMBIST_EN_ONLINE                         (1)        /* Enable online Local SRAM MBIST. */
+#define SMBIST_INSTANCE_ID                       (91)       /* Instance index for SMBIST. */
 
 #endif /* _PIC32CMJH01_SMBIST_INSTANCE_ */
