e7d0238dc1b38985b5ada6b939452f01d8cc1f47
Sumanth Naga Venkata Ranga Suraneni <Sumanth.Suraneni@microchip.com>
2023-04-18 18:07:51 +0530

    [AC] Regenerated demo for ac_ac_sleepwalk_singleshot_pic32cm_jh01_curiosity_pro
    MH3-77178

--- apps/ac/ac_sleepwalk_singleshot/firmware/src/config/pic32cm_jh01_curiosity_pro/definitions.h
+++ apps/ac/ac_sleepwalk_singleshot/firmware/src/config/pic32cm_jh01_curiosity_pro/definitions.h
@@ -57,8 +57,8 @@
 #include "peripheral/pm/plib_pm.h"
 #include "peripheral/sercom/usart/plib_sercom4_usart.h"
 #include "peripheral/supc/plib_supc.h"
-#include "peripheral/rtc/plib_rtc.h"
 #include "peripheral/ac/plib_ac.h"
+#include "peripheral/rtc/plib_rtc.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -67,6 +67,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32CM5164JH01100"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "PIC32CM"
+#define DEVICE_SERIES		 "PIC32CMJH01"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 48000000
