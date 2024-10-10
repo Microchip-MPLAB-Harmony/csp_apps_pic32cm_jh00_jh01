b3946fc30198160a08aba07b50ec0026e63e1c83
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-10 16:51:21 +0530

    [DSU] Regenerated demo for dsu__dsu_crc32_generate__firmware__pic32cm_jh01_curiosity_pro.X
    MH3-99680

--- apps/dsu/dsu_crc32_generate/firmware/src/packs/PIC32CM5164JH01100_DFP/pic32cm5164jh01100.h
+++ apps/dsu/dsu_crc32_generate/firmware/src/packs/PIC32CM5164JH01100_DFP/pic32cm5164jh01100.h
@@ -20,7 +20,7 @@
  *
  */
 
-/* File generated from device description version 2023-03-27T04:02:13Z */
+/* File generated from device description version 2023-07-04T09:15:55Z */
 #ifndef _PIC32CM5164JH01100_H_
 #define _PIC32CM5164JH01100_H_
 
