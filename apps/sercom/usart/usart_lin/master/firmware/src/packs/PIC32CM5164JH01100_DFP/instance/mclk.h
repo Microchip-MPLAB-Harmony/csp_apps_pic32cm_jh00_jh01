78639e53fef2c047292363defc72aa945f51ff97
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2024-10-10 16:57:28 +0530

    [SERCOM] Regenerated demo for sercom__usart__usart_lin__master__firmware__pic32cm_jh01_curiosity_pro.X
    MH3-99714

--- apps/sercom/usart/usart_lin/master/firmware/src/packs/PIC32CM5164JH01100_DFP/instance/mclk.h
+++ apps/sercom/usart/usart_lin/master/firmware/src/packs/PIC32CM5164JH01100_DFP/instance/mclk.h
@@ -20,7 +20,7 @@
  *
  */
 
-/* file generated from device description version 2023-03-27T04:02:13Z */
+/* file generated from device description version 2023-07-04T09:15:55Z */
 #ifndef _PIC32CMJH01_MCLK_INSTANCE_
 #define _PIC32CMJH01_MCLK_INSTANCE_
 
